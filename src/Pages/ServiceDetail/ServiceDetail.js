11933d99df459173e95ad7a87beae26d5984db99
atique-atq <atique.atq@gmail.com>
2022-11-09 18:32:47 +0600

    added review form

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const ServiceDetails = () => {
     const services = useLoaderData();
     const { name, description, price, img } = services;
+    const { user } = useContext(AuthContext);
+
     return (
         <div>
             <div class="grid grid-cols-2 gap-4 mx-20">
@@ -11,8 +14,8 @@ const ServiceDetails = () => {
                     <div className='grid grid-cols-2 gap-4'>
                         <div className='flex flex-col justify-center text-gray-500 font-semibold'>
                             <img src={img} className='w-11/12' alt="" />
-                            <p>Service Name: {name}</p>
-                            <p>Fee: {price}</p>
+                            <p>{name}</p>
+                            <small>Fee: {price}</small>
 
                         </div>
                         <div>
@@ -24,7 +27,27 @@ const ServiceDetails = () => {
                     </div>
                 </div>
 
-                <div>Review Section</div>
+                <div>
+                    <h3 className='underline underline-offset-4 text-green-500 font-bold text-xl mb-2 text-center'>Review</h3>
+                    <form className="card-body">
+                        {
+                            user?.email ?
+                                <div>
+                                    <div className="form-control">
+                                        <textarea type="text" name='review-text' placeholder=" add a review" className="input input-bordered" required />
+                                    </div>
+                                    <div className="form-control mt-6">
+                                        <button className="btn btn-info hover:bg-success" type="submit">Submit Review</button>
+                                    </div>
+                                </div>
+                                :
+                                <p>Please login to add a review</p>
+                        }
+
+
+                    </form>
+
+                </div>
             </div>
         </div >
     );
