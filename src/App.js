6967540a553f449347fb5bf8c443ef605bf1715f
atique-atq <atique.atq@gmail.com>
2022-11-09 08:23:15 +0600

    added full image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './Router/Routes/Routes';
 import { Toaster } from "react-hot-toast";
+import 'react-photo-view/dist/react-photo-view.css';
 import './App.css';
 
 function App() {
--- src/Pages/AllServices/AllServicesSingleCard.js
+++ src/Pages/AllServices/AllServicesSingleCard.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 
 const AllServicesSingleCard = ({ service }) => {
     const { name, description, price, img } = service;
+    const images = [img]
+
     return (
         <div>
             <div className="card w-11/12 glass shadow-2xl ml-5">
-                <figure><img className='h-72 w-full' src={img} alt="car!" /></figure>
+                <PhotoProvider>
+                    <div className="foo">
+                        {images.map((item, index) => (
+                            <PhotoView key={index} src={item}>
+                                <figure><img className='h-72 w-full' src={item} alt="doctor!" /></figure>
+                            </PhotoView>
+                        ))}
+                    </div>
+                </PhotoProvider>
                 <div className="card-body">
                     <h2 className="card-title">{name}</h2>
                     <p>{description}</p>
