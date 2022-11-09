5baafdf24233d716cdba8d591132a0a5114892b6
atique-atq <atique.atq@gmail.com>
2022-11-09 17:48:44 +0600

    added service details

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const ServiceDetails = () => {
+    const services = useLoaderData();
+    const { name, description, price, img } = services;
     return (
         <div>
-            <h1>This is service details</h1>
-        </div>
+            <div class="grid grid-cols-2 gap-4 mx-20">
+                <div>
+                    <div className='grid grid-cols-2 gap-4'>
+                        <div className='flex flex-col justify-center text-gray-500 font-semibold'>
+                            <img src={img} className='w-11/12' alt="" />
+                            <p>Service Name: {name}</p>
+                            <p>Fee: {price}</p>
+
+                        </div>
+                        <div>
+                            <h3 className='underline underline-offset-4 text-green-500 font-bold text-xl mb-2'>
+                                Service Detail</h3>
+                            <p className='text-gray-500 text-sm'>{description}</p>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div>Review Section</div>
+            </div>
+        </div >
     );
 };
 
