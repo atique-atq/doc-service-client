7d6db7e8e901ecc519b3af167bb2fb583cd0931d
atique-atq <atique.atq@gmail.com>
2022-11-10 13:43:14 +0600

    deleted

--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -34,19 +34,28 @@ const MyReviews = () => {
 
     return (
         <div>
-            <div>
-                <h2 className="text-xl text-gray-600 font-bold text-center mb-3">You have {myReviews?.length} Reviews</h2>
-                <div className='grid gap-6 grid-cols-1 lg:grid-cols-3'>
-                    {
-                        myReviews.map(review => <MyReviewRow
-                            key={review._id}
-                            reviewRow={review}
-                            handleDelete={handleDelete}
-                        // handleStatusUpdate={handleStatusUpdate}
-                        ></MyReviewRow>)
-                    }
-                </div>
-            </div>
+            {
+                (myReviews.length > 0) ?
+                    <div>
+                        <h2 className="text-xl text-gray-600 font-bold text-center mb-3">You have {myReviews?.length} Reviews</h2>
+                        <div className='grid gap-6 grid-cols-1 lg:grid-cols-3'>
+                            {
+                                myReviews.map(review => <MyReviewRow
+                                    key={review._id}
+                                    reviewRow={review}
+                                    handleDelete={handleDelete}
+                                // handleStatusUpdate={handleStatusUpdate}
+                                ></MyReviewRow>)
+                            }
+                        </div>
+                    </div>
+                    :
+                    <div className='bg-red-200 mx-22 h-6/12'>
+                        <h1 className='my-auto px-52 py-52 text-2xl font-bold text-center'>No reviews were added</h1>
+                    </div>
+
+            }
+
         </div >
     );
 };
