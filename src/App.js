3ad4f1dba54a162e8c9d7f024e7189fa23f5d3e7
atique-atq <atique.atq@gmail.com>
2022-12-31 13:00:16 +0600

    margin resized

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import { RouterProvider } from 'react-router-dom';
-import router from './Router/Routes/Routes';
+import { RouterProvider } from "react-router-dom";
+import router from "./Router/Routes/Routes";
 import { Toaster } from "react-hot-toast";
-import 'react-photo-view/dist/react-photo-view.css';
-import './App.css';
+import "react-photo-view/dist/react-photo-view.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="max-w-screen-xl mx-auto lg:mx-8">
+    <div className="max-w-screen-xl mx-auto">
       <RouterProvider router={router}></RouterProvider>
       <Toaster></Toaster>
-      
     </div>
   );
 }
--- src/Pages/AllServices/AllServicesSingleCard.js
+++ src/Pages/AllServices/AllServicesSingleCard.js
@@ -1,34 +1,36 @@
-import React from 'react';
-import { PhotoProvider, PhotoView } from 'react-photo-view';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import { Link } from "react-router-dom";
 
 const AllServicesSingleCard = ({ service }) => {
-    const { _id, name, description, price, img } = service;
-    const images = [img]
+  const { _id, name, description, price, img } = service;
+  const images = [img];
 
-    return (
-        <div>
-            <div className="card h-full w-11/12 glass shadow-2xl ml-5">
-                <PhotoProvider>
-                    <div className="foo">
-                        {images.map((item, index) => (
-                            <PhotoView key={index} src={item}>
-                                <figure><img className='h-72 w-full' src={item} alt="doctor!" /></figure>
-                            </PhotoView>
-                        ))}
-                    </div>
-                </PhotoProvider>
-                <div className="card-body">
-                    <h2 className="card-title">{name}</h2>
-                    <p>{description.slice(0, 99) + '...'}</p>
-                    <p className='font-bold text-gray-500'>Fee: {price}</p>
-                    <Link to={`/service/${_id}`} className="card-actions justify-end">
-                        <button className="btn btn-primary">View Details</button>
-                    </Link>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="card h-full w-11/12 glass shadow-2xl ml-5">
+        <PhotoProvider>
+          <div className="foo">
+            {images.map((item, index) => (
+              <PhotoView key={index} src={item}>
+                <figure>
+                  <img className="h-72 w-full" src={item} alt="doctor!" />
+                </figure>
+              </PhotoView>
+            ))}
+          </div>
+        </PhotoProvider>
+        <div className="card-body">
+          <h2 className="card-title">{name}</h2>
+          <p>{description.slice(0, 99) + "..."}</p>
+          <p className="font-bold text-gray-500">Fee: {price}</p>
+          <Link to={`/service/${_id}`} className="card-actions justify-end">
+            <button className="btn btn-primary">View Details</button>
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AllServicesSingleCard;
